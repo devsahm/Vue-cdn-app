59e13fb7b8c7631095ad60a3753d96ee79d896de
unknown <devsahm>
2022-02-24 07:04:08 +0100

    remove console logs

--- app.js
+++ app.js
@@ -13,14 +13,13 @@ const app =  Vue.createApp({
       async  getUser() {
         const res = await fetch('https://randomuser.me/api')
         const { results } = await res.json()
-        console.log(results);
-
+        
         this.firstname = results[0].name.first
         this.lastname  = results[0].name.last
         this.email = results[0].email
         this.gender = results[0].gender,
         this.picture = results[0].picture.large
-        
+
         }
     
     },
